652c4823677c4179070560ae8057816db39f4968
Omkar Kaul <omkar@xtra.co.nz>
2020-04-24 15:13:30 +1200

    update for testing

--- proxy.js
+++ proxy.js
@@ -6,31 +6,45 @@ const app = express();
 app.get('/:plate', async (req, res) => {
     const plate = req.params.plate;
 
-    const availability = await getPlateAvailability(plate);
-    const content = await getPlateContent(plate);
+    const availabilityResponse = await getPlateAvailability(plate);
+	const contentResponse = await getPlateContent(plate);
 
-    const data = {
-        "availability" : availability,
-        "content" : content
-    };
-
-    res.header("Access-Control-Allow-Origin", "*");
-    res.send(data);
+	if (availabilityResponse.status === 200 && contentResponse.status === 200) {
+		const data = {
+			"availability" : availabilityResponse.available,
+			"content" : contentResponse.data
+		};
+	
+		res.header('Access-Control-Allow-Origin', '*')
+		res.json(data);
+	} else {
+		res.header('Access-Control-Allow-Origin', '*')
+		res.send("data unavailable");
+	}
 });
 
 async function getPlateAvailability(plate) {
-    const res = await fetch(`https://api.kiwiplates.nz/api//combination/${plate}`);
-    const json = await res.json();
-    const available = json.Data.Available;
+	const res = await fetch(`https://api.kiwiplates.nz/api//combination/${plate}`);
+	
+	if (res.ok) {
+		const json = await res.json();
+    	const available = json.Data.Available;
 
-    return available;
+    	return {status: 200, "available" : available}
+	} else {
+		return {status: 500}
+	}
 };
 
 async function getPlateContent(plate) {
     const res = await fetch(`https://www.carjam.co.nz/a/rvid_service::rnr?plate=${plate}`);
-    const json = await res.json();
-
-    return json;
+	
+	if (res.ok) {
+		const json = await res.json();
+    	return { status: 200, "data": json}
+	} else {
+		return {status: 500}
+	}
 };
 
 app.listen(8080, () => {
